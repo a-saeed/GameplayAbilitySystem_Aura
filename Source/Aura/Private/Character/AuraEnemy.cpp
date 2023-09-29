8743798e30ace43e1fefd54c0c255b196646a2e8
Blue_Gene <khalifaabdallah101@gmail.com>
2023-09-30 00:39:11 +0300

    Post Process Highlight

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -2,13 +2,30 @@
 
 
 #include "Character/AuraEnemy.h"
+#include "Aura/Aura.h"
+
+AAuraEnemy::AAuraEnemy()
+{
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+}
 
 void AAuraEnemy::HighlightActor()
 {
-	
+	if(GetMesh() && Weapon)
+	{
+		GetMesh()->SetRenderCustomDepth(true);
+		GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+		
+		Weapon->SetRenderCustomDepth(true);
+		Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	}
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
-	
+	if(GetMesh() && Weapon)
+	{
+		GetMesh()->SetRenderCustomDepth(false);
+		Weapon->SetRenderCustomDepth(false);
+	}
 }
